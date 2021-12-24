c69465382241ba5b675449824a37682597d9f094
AlexeyIvanov <AXSoran47@yandex.ru>
2021-12-24 18:49:47 +0300

    Main imports are done. The Bone of the project are created. Main game loop is also now added.

---
+++ main.py
@@ -0,0 +1,34 @@
+# imports
+import pygame
+import time as t
+import sys
+import random as r
+import sqlite3 as sql
+# /imports
+
+
+class Entity:
+    pass
+
+
+class Character:
+    pass
+
+
+class Laser:
+    pass
+
+
+class Enemy:
+    pass
+
+
+if (__name__ == '__main__'):
+    pygame.init()
+    screen = pygame.display.set_mode((700, 700))
+    pygame.display.set_caption('LongLiveTheKing')
+    screen.fill((0, 0, 0))
+    running = True
+    while running:
+        pass
+    pygame.quit()
