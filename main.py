ce0eed0e441ae77d4fc6218de9eca90fabd0e6aa
Sor4nex <AXSoran47@yandex.ru>
2022-01-07 18:39:04 +0300

    Bullets added

--- main.py
+++ main.py
@@ -8,44 +8,52 @@ import sqlite3 as sql
 
 
 # classes of game objects
-class Entity:  # parent class
-    def __init__(self, x_pos, y_pos, x_width, y_width, type_scucture=None) -> None:
-        self.pos_entity_x = x_pos
-        self.pos_entity_y = y_pos
-        self.x_width = x_width
-        self.y_width = y_width
-        self.type_structure = type_scucture
-
-    def show_entity_game(self, screen) -> None:  # shows all objects of game, you can interact with
-        if (self.type_structure is None):
-            pygame.draw.ellipse(screen, (255, 0, 0), (self.pos_entity_x, self.pos_entity_y, self.x_width, self.y_width))
-
-    def set_new_entity_pos(self, pos_x=0, pos_y=0) -> None: # updates entity position (self.pos_entity_x = x_pos, self.pos_entity_y = y_pos)
-        self.pos_entity_x += pos_x
-        self.pos_entity_y += pos_y
-
-
-class Character(Entity):  # A player character
-    def __init__(self, x_pos, y_pos, x_width, y_width, skin) -> None:
-        super().__init__(x_pos, y_pos, x_width, y_width)
+class Character:  # A player character
+    def __init__(self, x_pos, y_pos, width, skin) -> None:
         self.player_skin = skin
+        self.x = x_pos
+        self.y = y_pos
+        self.width = width
+    
+    def show_entity_game(self, screen):
+        pygame.draw.circle(screen, (0, 255, 0), (self.x, self.y), self.width)
+
+    def set_new_entity_pos(self, plus_x, plus_y):
+        self.x += plus_x
+        self.y += plus_y
 
 
-class Laser:  # A laser, that appears during the game
-    def __init__(self, start_x, start_y, end_x, end_y) -> None:
-        self.start_x = start_x
-        self.start_y = start_y
-        self.end_x = end_x
-        self.end_y = end_y
+class Bullet:  # A laser, that appears during the game
+    def __init__(self) -> None:
+        self.radio = 5
+        start_x_choice = r.choice([0, 1, 2, 3])
+        if (start_x_choice == 0):
+            self.x, self.y = (0, r.randint(0, 500))
+            self.speed_x = r.randint(0, 10)
+            self.speed_y = r.randint(-5, 5)
+        elif (start_x_choice == 1):
+            self.x, self.y = (r.randint(0, 500), 0)
+            self.speed_x = r.randint(-5, 5)
+            self.speed_y = r.randint(0, 10)
+        elif (start_x_choice == 2):
+            self.x, self.y = (500, r.randint(0, 500))
+            self.speed_x = r.randint(-10, 0)
+            self.speed_y = r.randint(-5, 5)
+        elif (start_x_choice == 3):
+            self.x, self.y = (r.randint(0, 500), 500)
+            self.speed_x = r.randint(-5, 5)
+            self.speed_y = r.randint(-10, 0)
 
-    def check_hit_laser(self) -> bool:  # checks if smth stands in way of laser of touches it
+
+    def show_entity_game(self, screen) -> None:
+        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), self.radio)
+    
+    def set_new_entity_pos(self):
+        self.x += self.speed_x
+        self.y += self.speed_y
+
+    def check_hit_bullet(self) -> bool:  # checks if smth stands in way of laser of touches it
         pass
-
-
-class Enemy(Entity):  # A chasing enemies
-    def __init__(self, x_pos, y_pos, x_width, y_width, type_structure) -> None:
-        super().__init__(x_pos, y_pos, x_width, y_width, type_structure)
-        self.type_structure = type_structure
 # /classes of game objects
 
 
@@ -53,7 +61,7 @@ class Enemy(Entity):  # A chasing enemies
 if (__name__ == '__main__'):
     # window set up
     pygame.init()
-    screen = pygame.display.set_mode((1000, 900))
+    screen = pygame.display.set_mode((500, 500))
     pygame.display.set_caption('LongLiveTheKing')
     screen.fill((0, 0, 0))
     # /window set up
@@ -65,13 +73,15 @@ if (__name__ == '__main__'):
     moving_x_down = False
     moving_y_down = False
     clock = pygame.time.Clock()
-    objects_on_screen = list()
+    bullets_on_screen = list()
     counter = 0
+    time_was = 0
+    time_now = 0
+    bullet_count = 1
     # /variables
 
     # game set up
-    player_character = Character(350, 350, 30, 20, 'Normal')
-    objects_on_screen.append(player_character)
+    player_character = Character(350, 350, 8, 'Normal')
     # game set up
 
     # game
@@ -105,9 +115,26 @@ if (__name__ == '__main__'):
             player_character.set_new_entity_pos(0, -8)
         if (moving_y_down):
             player_character.set_new_entity_pos(0, 8)
-        for object in objects_on_screen:
-            object.show_entity_game(screen)
+        if (time_now % 10 == 0) and (time_now != 0) and (time_now != time_was):
+            bullet_count += 1
+        if (time_now != time_was):
+            for bullet in range(bullet_count):
+                new = Bullet()
+                bullets_on_screen.append(new)
+        for bullet in bullets_on_screen:
+            bullet.set_new_entity_pos()
+            if ((player_character.x > bullet.x) and (player_character.x < bullet.x + 15)) and (player_character.y > bullet.y) and (player_character.y < bullet.y + 15):
+                print('lose')
+            if (bullet.x > 500) or (bullet.y > 500) or (bullet.x < 0) or (bullet.y < 0):
+                bullets_on_screen.remove(bullet)
+            bullet.show_entity_game(screen)
+        player_character.show_entity_game(screen)
         clock.tick(60)
+        time_was = time_now
+        counter += 1
+        if (counter % 60 == 0):
+            time_now = counter // 60
+            print(time_now)
         pygame.display.flip()
         screen.fill((0, 0, 0))
     pygame.quit()
