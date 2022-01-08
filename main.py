1c82fdb93eecede691e1b2fa605312b26aa2fee3
Sor4nex <AXSoran47@yandex.ru>
2022-01-08 19:06:38 +0300

    ingame interface is made. Some boosters functions added. Data Base created. Now records and money can be tracked

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import pygame
 import time as t
 import sys
 import random as r
-import sqlite3 as sql
+import sqlite3
 
 from pygame import image
+from pygame.font import Font
+from pygame.transform import scale
 # /imports
 
 
@@ -43,8 +45,13 @@ def game_rules(screen):
         clock.tick(50)
 
 def game_start_screen(screen) -> bool:
+    global con, cur
     image = pygame.image.load('photo/start.png')
     screen.blit(image, (0, 0))
+    font = pygame.font.Font(None, 50)
+    record_from_db = cur.execute('SELECT record FROM user WHERE id = 1').fetchall()[0][0]
+    record = font.render(str(record_from_db), False, (255, 0, 0))
+    screen.blit(record, (500, 75))
     while True:
         for event in pygame.event.get():
             if (event.type == pygame.QUIT):
@@ -57,18 +64,24 @@ def game_start_screen(screen) -> bool:
                     game_boost_shop(screen)
                     image = pygame.image.load('photo/start.png')
                     screen.blit(image, (0, 0))
+                    screen.blit(record, (500, 75))
                 elif ((x > 658) and (x < 982)) and ((y > 242) and (y < 336)):
                     game_rules(screen)
                     image = pygame.image.load('photo/start.png')
                     screen.blit(image, (0, 0))
+                    screen.blit(record, (500, 75))
                 elif ((x > 39) and (x < 135)) and ((y > 415) and (y < 446)):
                     return False
         pygame.display.flip()
         clock.tick(50)
 
 def game_death_screen(screen) -> bool:
+    global time_now
+    f2 = pygame.font.Font(None, 70)
+    result = f2.render(str(time_now), False, (255, 255, 255))
     image = pygame.image.load('photo/death.png')
     screen.blit(image, (0, 0))
+    screen.blit(result, (470, 200))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -117,15 +130,15 @@ class Bullet:  # A laser, that appears during the game
             self.speed_x = r.randint(0, 10)
             self.speed_y = r.randint(-5, 5)
         elif (start_x_choice == 1):
-            self.x, self.y = (r.randint(0, 1000), 0)
+            self.x, self.y = (r.randint(0, 700), 0)
             self.speed_x = r.randint(-5, 5)
             self.speed_y = r.randint(0, 10)
         elif (start_x_choice == 2):
-            self.x, self.y = (1000, r.randint(0, 500))
+            self.x, self.y = (700, r.randint(0, 500))
             self.speed_x = r.randint(-10, 0)
             self.speed_y = r.randint(-5, 5)
         elif (start_x_choice == 3):
-            self.x, self.y = (r.randint(0, 1000), 500)
+            self.x, self.y = (r.randint(0, 700), 500)
             self.speed_x = r.randint(-5, 5)
             self.speed_y = r.randint(-10, 0)
 
@@ -151,6 +164,10 @@ if (__name__ == '__main__'):
     pygame.init()
     screen = pygame.display.set_mode((1000, 500))
     app_running = True
+    con = sqlite3.connect('db/game.db')
+    cur = con.cursor()
+    f1 = pygame.font.Font(None, 50)
+    f2 = pygame.font.Font(None, 25)
     pygame.display.set_caption('LongLiveTheKing')
     screen.fill((0, 0, 0))
     # /window set up
@@ -167,7 +184,16 @@ if (__name__ == '__main__'):
         counter = 0
         time_was = 0
         time_now = 0
+        money_earned = 0
         bullet_count = 3
+        img_fre = pygame.image.load('photo/fre.png')
+        img_cle = pygame.image.load('photo/cle.png')
+        img_rel = pygame.image.load('photo/rel.png')
+        img_fre = pygame.transform.scale(img_fre, (50, 50))
+        img_cle = pygame.transform.scale(img_cle, (50, 50))
+        img_rel = pygame.transform.scale(img_rel, (50, 50))
+        record_now = cur.execute('SELECT record FROM user WHERE id = 1').fetchall()[0][0]
+        freeze, clean, reload = cur.execute('SELECT freeze, clean, reload FROM boosts WHERE id = 1').fetchall()[0]
         # /variables
 
         # game set up
@@ -179,6 +205,7 @@ if (__name__ == '__main__'):
         if (app_running == False):
             break
         while (running):
+            # events
             for event in pygame.event.get():  # event checker. Get all events, done by player
                 if (event.type == pygame.QUIT):
                     running = False
@@ -192,6 +219,10 @@ if (__name__ == '__main__'):
                             moving_y_up = True
                     if (event.key == pygame.K_s):
                             moving_y_down = True
+                    if (event.key == pygame.K_SPACE):
+                        if (clean != 0):
+                            clean -= 1
+                            bullets_on_screen.clear()
                 if (event.type == pygame.KEYUP):
                     if (event.key == pygame.K_d):
                         moving_x_up = False
@@ -201,38 +232,85 @@ if (__name__ == '__main__'):
                         moving_y_up = False
                     if (event.key == pygame.K_s):
                         moving_y_down = False
+            # /events
+            
+            # player movements
             if (moving_x_up):
-                player_character.set_new_entity_pos(8, 0)
+                if (player_character.x + 8 < 700):
+                    player_character.set_new_entity_pos(8, 0)
             if (moving_x_down):
-                player_character.set_new_entity_pos(-8, 0)
+                if (player_character.x - 8 > 0):
+                    player_character.set_new_entity_pos(-8, 0)
             if (moving_y_up):
-                player_character.set_new_entity_pos(0, -8)
+                if (player_character.y - 8 > 0):
+                    player_character.set_new_entity_pos(0, -8)
             if (moving_y_down):
-                player_character.set_new_entity_pos(0, 8)
+                if (player_character.y + 8 < 500):
+                    player_character.set_new_entity_pos(0, 8)
+            # /player movements
+
+            # time checker
             if (time_now % 10 == 0) and (time_now != 0) and (time_now != time_was):
                 bullet_count += 2
+                money_earned += 10
             if (time_now != time_was):
                 for bullet in range(bullet_count):
                     new = Bullet()
                     bullets_on_screen.append(new)
+            # /time checker
+
+            # bullet movements and death
             for bullet in bullets_on_screen:
                 bullet.set_new_entity_pos()
-                if ((player_character.x > bullet.x) and (player_character.x < bullet.x + 20)) and (player_character.y > bullet.y) and (player_character.y < bullet.y + 20):
+                if ((player_character.x > bullet.x - 13) and (player_character.x < bullet.x + 13)) and (player_character.y > bullet.y - 13) and (player_character.y < bullet.y + 13):
+                    new_record = time_now
+                    cur.execute('UPDATE user SET record = ? WHERE record < ?', (new_record, new_record))
+                    cur.execute('UPDATE user SET money = money + ? WHERE id = 1', (money_earned, ))
+                    con.commit()
                     running = False
                     app_running = game_death_screen(screen)
                     break
-                if (bullet.x > 1000) or (bullet.y > 500) or (bullet.x < 0) or (bullet.y < 0):
+                if (bullet.x > 705) or (bullet.y > 505) or (bullet.x < -5) or (bullet.y < -5):
                     bullets_on_screen.remove(bullet)
                 bullet.show_entity_game(screen)
+            # /bullet movements and death
+
+            # rendering
             player_character.show_entity_game(screen)
             clock.tick(60)
             time_was = time_now
             counter += 1
             if (counter % 60 == 0):
                 time_now = counter // 60
-                print(time_now)
+            timer = f1.render(str(time_now), False, (255, 255, 255))
+            text1 = f1.render('Время:', False, (255, 255, 255))
+            text2 = f1.render('Рекорд:', False, (0, 255, 0))
+            text3 = f2.render('Пуль в секунду:', False, (255, 0, 0))
+            text4 = f2.render('Монет получено:', False, (255, 207, 64))
+            record_text = f1.render(str(record_now), False, (0, 255, 0))
+            money_text = f1.render(str(money_earned), False, (255, 207, 64))
+            bullet_text = f1.render(str(bullet_count), False, (255, 0, 0))
+            cle = f1.render(str(clean), False, (255, 255, 255))
+            fre = f1.render(str(freeze), False, (255, 255, 255))
+            rel = f1.render(str(reload), False, (255, 255, 255))
+            screen.blit(text2, (720, 60))
+            screen.blit(text1, (720, 10))
+            screen.blit(text3, (720, 330))
+            screen.blit(text4, (720, 380))
+            screen.blit(timer, (900, 10))
+            screen.blit(record_text, (900, 60))
+            screen.blit(money_text, (900, 380))
+            screen.blit(bullet_text, (900, 330))
+            screen.blit(cle, (900, 110))
+            screen.blit(fre, (900, 190))
+            screen.blit(rel, (900, 270))
+            screen.blit(img_cle, (720, 110))
+            screen.blit(img_fre, (720, 190))
+            screen.blit(img_rel, (720, 270))
+            pygame.draw.line(screen, (255, 0, 0), (700, 0), (700, 500), 3)
             pygame.display.flip()
             screen.fill((0, 0, 0))
+            # /rendering
     pygame.quit()
     # /game
 # /game main script
