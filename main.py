89950edb932e377f06f8f282f2223f7df9871f32
Sor4nex <AXSoran47@yandex.ru>
2022-01-12 22:58:48 +0300

    A reflecting block was added.

--- main.py
+++ main.py
@@ -134,8 +134,12 @@ def game_death_screen(screen) -> bool:
                 x, y = event.pos
                 if ((x > 46) and (x < 350)) and ((y > 321) and (y < 397)):
                     return True
+                else:
+                    pass
                 if ((x > 631) and (x < 936)) and ((y > 321) and (y < 397)):
                     return False
+                else:
+                    pass
         pygame.display.flip()
         clock.tick(50)
 # /other functions
@@ -170,21 +174,43 @@ class Bullet:  # A laser, that appears during the game
         self.paused = False
         start_x_choice = r.choice([0, 1, 2, 3])
         if (start_x_choice == 0):
-            self.x, self.y = (0, r.randint(0, 500))
-            self.speed_x = r.randint(0, 10)
-            self.speed_y = r.randint(-5, 5)
+            x = 0
+            y = r.randint(0, 500)
+            speed_y = r.randint(-5, 5)
+            speed_x = r.randint(0, 10)
+            self.x = x
+            self.y = y
+            self.speed_x = speed_x
+            self.speed_y = speed_y
         elif (start_x_choice == 1):
-            self.x, self.y = (r.randint(0, 700), 0)
-            self.speed_x = r.randint(-5, 5)
-            self.speed_y = r.randint(0, 10)
+            x = r.randint(0, 700)
+            y = 0
+            speed_x = r.randint(-5, 5)
+            speed_y = r.randint(0, 10)
+            self.x = x
+            self.y = y
+            self.speed_x = speed_x
+            self.speed_y = speed_y
         elif (start_x_choice == 2):
-            self.x, self.y = (700, r.randint(0, 500))
-            self.speed_x = r.randint(-10, 0)
-            self.speed_y = r.randint(-5, 5)
+            x = 700
+            y = r.randint(0, 500)
+            speed_x = r.randint(-10, 0)
+            speed_y = r.randint(-5, 5)
+            self.x = x
+            self.y = y
+            self.speed_x = speed_x
+            self.speed_y = speed_y
         elif (start_x_choice == 3):
-            self.x, self.y = (r.randint(0, 700), 500)
-            self.speed_x = r.randint(-5, 5)
-            self.speed_y = r.randint(-10, 0)
+            x = r.randint(0, 700)
+            y = 500
+            speed_y = r.randint(-10, 0)
+            speed_x = r.randint(-5, 5)
+            self.x = x
+            self.y = y
+            self.speed_x = speed_x
+            self.speed_y = speed_y
+        else:
+            pass
 
 
     def show_entity_game(self, screen) -> None:
@@ -199,6 +225,39 @@ class Bullet:  # A laser, that appears during the game
 
     def pause_motion(self, bool) -> None:
         self.paused = bool
+    
+    def reflect(self):
+        speed_x = self.speed_x
+        speed_y = self.speed_y
+        self.speed_x = speed_y
+        self.speed_y = speed_x
+
+
+class ReflectBlock:
+    def __init__(self):
+        start_x_choice = r.choice([0, 1, 2, 3])
+        if (start_x_choice == 0):
+            self.x = 200
+            self.y = 150
+            self.width = 30
+        elif (start_x_choice == 1):
+            self.x = 500
+            self.y = 150
+            self.width = 30
+        elif (start_x_choice == 2):
+            self.x = 200
+            self.y = 350
+            self.width = 30
+        elif (start_x_choice == 3):
+            self.x = 500
+            self.y = 350
+            self.width = 30
+        else:
+            pass
+
+
+    def show_entity_game(self, screen):
+        pygame.draw.rect(screen, (100, 200, 100), (self.x, self.y, self.width, self.width))
 # /classes of game objects
 
 
@@ -218,39 +277,47 @@ if (__name__ == '__main__'):
     screen.fill((0, 0, 0))
     # /window set up
     
-    while app_running:
+    while (app_running):
         app_running = game_start_screen(screen)
+
         # variables
         running = True
         moving_x_up = False
         moving_y_up = False
         moving_x_down = False
         moving_y_down = False
+        game_paused = False
+        stop = False
+
         bullets_on_screen = list()
+
         counter = 0
         time_was = 0
         time_now = 0
         money_earned = 0
         bullet_count = 3
         pause_countdown = 4
-        game_paused = False
+
         img_fre = pygame.image.load('photo/fre.png')
         img_cle = pygame.image.load('photo/cle.png')
         img_rel = pygame.image.load('photo/rel.png')
         img_fre = pygame.transform.scale(img_fre, (50, 50))
         img_cle = pygame.transform.scale(img_cle, (50, 50))
         img_rel = pygame.transform.scale(img_rel, (50, 50))
+
         record_now = cur.execute('SELECT record FROM user WHERE id = 1').fetchall()[0][0]
         freeze, clean, reload = cur.execute('SELECT freeze, clean, reload FROM boosts WHERE id = 1').fetchall()[0]
         # /variables
 
         # game set up
         player_character = Character(350, 350, 8, 'Normal')
+        block = ReflectBlock()
         # game set up
 
         # game
         if (app_running == False):
             break
+
         while (running):
             # events
             for event in pygame.event.get():  # event checker. Get all events, done by player
@@ -270,10 +337,14 @@ if (__name__ == '__main__'):
                         if (clean != 0):
                             clean -= 1
                             bullets_on_screen.clear()
+                        else:
+                            pass
                     if (event.key == pygame.K_r):
                         if (reload != 0) and (bullet_count >= 3):
                             reload -= 1
                             bullet_count -= 2
+                        else:
+                            pass
                     if (event.key == pygame.K_f):
                         if (freeze != 0):
                             freeze -= 1
@@ -281,6 +352,8 @@ if (__name__ == '__main__'):
                             pause_countdown = 3
                             pause_countdown_text = fre_font.render(str(pause_countdown), False, (0, 0, 255))
                             screen.blit(pause_countdown_text, (670, 30))
+                        else:
+                            pass
                 if (event.type == pygame.KEYUP):
                     if (event.key == pygame.K_d):
                         moving_x_up = False
@@ -295,22 +368,35 @@ if (__name__ == '__main__'):
             # player movements
             if (moving_x_up):
                 if (player_character.x + 8 < 700):
-                    player_character.set_new_entity_pos(8, 0)
+                    if (((player_character.x + 8 < block.x) or (player_character.x + 8> block.x + 39)) or ((player_character.y < block.y) or (player_character.y > block.y + 39))):
+                        player_character.set_new_entity_pos(8, 0)
+                    else:
+                        pass
             if (moving_x_down):
                 if (player_character.x - 8 > 0):
-                    player_character.set_new_entity_pos(-8, 0)
+                    if (((player_character.x - 8 < block.x) or (player_character.x - 8 > block.x + 39)) or ((player_character.y < block.y) or (player_character.y > block.y + 39))):
+                        player_character.set_new_entity_pos(-8, 0)
+                    else:
+                        pass
             if (moving_y_up):
                 if (player_character.y - 8 > 0):
-                    player_character.set_new_entity_pos(0, -8)
+                    if (((player_character.x < block.x) or (player_character.x > block.x + 39)) or ((player_character.y - 8 < block.y) or (player_character.y - 8 > block.y + 39))):
+                        player_character.set_new_entity_pos(0, -8)
+                    else:
+                        pass
             if (moving_y_down):
                 if (player_character.y + 8 < 500):
-                    player_character.set_new_entity_pos(0, 8)
+                    if (((player_character.x < block.x) or (player_character.x > block.x + 39)) or ((player_character.y + 8 < block.y) or (player_character.y + 8 > block.y + 39))):
+                        player_character.set_new_entity_pos(0, 8)
+                    else:
+                        pass
             # /player movements
 
             # time checker
             if (time_now % 10 == 0) and (time_now != 0) and (time_now != time_was):
                 bullet_count += 2
                 money_earned += 10
+
             if (time_now != time_was):
                 if not(game_paused):
                     for bullet in range(bullet_count):
@@ -323,6 +409,7 @@ if (__name__ == '__main__'):
             # bullet movements and death
             if (pause_countdown == 0):
                 game_paused = False
+
             for bullet in bullets_on_screen:
                 if not(game_paused):
                     bullet.set_new_entity_pos()
@@ -334,14 +421,21 @@ if (__name__ == '__main__'):
                     con.commit()
                     running = False
                     app_running = game_death_screen(screen)
+                    stop = True
                     break
+                if ((bullet.x > block.x) and (bullet.x < block.x + 30)) and ((bullet.y > block.y) and (bullet.y < block.y + 30)):
+                    bullet.reflect()
                 if (bullet.x > 705) or (bullet.y > 505) or (bullet.x < -5) or (bullet.y < -5):
                     bullets_on_screen.remove(bullet)
                 bullet.show_entity_game(screen)
+
+            if (stop):
+                break
             # /bullet movements and death
 
             # rendering
             player_character.show_entity_game(screen)
+            block.show_entity_game(screen)
             clock.tick(60)
             time_was = time_now
             counter += 1
